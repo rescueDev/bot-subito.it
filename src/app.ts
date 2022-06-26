02267fdc5982f736f3cf891b96a965b8523ebdc1
sa-borgia3 <sa.borgia3@gmail.com>
2022-06-26 18:34:42 +0200

    bot working but still work in progress

---
+++ src/app.ts
@@ -0,0 +1,140 @@
+import "dotenv/config";
+import Scraper from "./scraper";
+
+var category: string;
+var query: string;
+var page_current: number = 1;
+
+const TelegramBot = require("node-telegram-bot-api");
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+var answerCallbacks = {};
+
+bot.on("message", function (message) {
+  var callback = answerCallbacks[message.chat.id];
+  if (callback) {
+    delete answerCallbacks[message.chat.id];
+    return callback(message);
+  }
+});
+
+bot.onText(/\/start/, (msg, match) => {
+  bot.sendMessage(
+    msg.chat.id,
+    "Ciao sono il tuo bot Subito Scraper, scegli uno dei comandi di seguito"
+  );
+});
+
+// Matches "/echo [whatever]"
+// bot.onText(/\/cerca/, (msg, match) => {
+//   bot.sendMessage(msg.chat.id, "Dimmi cosa cerchi").then(function () {
+//     answerCallbacks[msg.chat.id] = function (answer) {
+//       query = answer.text;
+//       console.log("query", query);
+
+//       /* options category */
+//       let options = {
+//         reply_markup: JSON.stringify({
+//           inline_keyboard: JSON.parse(Scraper.CreateJsonOption()),
+//         }),
+//       };
+
+//       /* step category */
+//       bot
+//         .sendMessage(msg.chat.id, "Scegli una di queste categorie:", options)
+//         .then(function (res) {
+//           bot.on("callback_query", function onCallbackQuery(callbackQuery) {
+//             category = callbackQuery.data;
+
+//             let scraper = new Scraper(query, category);
+//             let url = scraper.url;
+
+//             console.log("full url", url);
+//             fetchResults(url, msg.chat.id);
+//           });
+//         });
+//     };
+//   });
+// });
+
+//delayed sending messages, prevent too many request telegram server
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+async function SendDelayedMessages(links: string[], chatID) {
+  for (let index = 0; index < links.length; index++) {
+    const link = links[index];
+    bot.sendMessage(chatID, link);
+    await timer(1500);
+  }
+}
+
+function SetCategory(chatID: any, page: number = 1) {
+  bot.on("callback_query", function onCallbackQueryCategory(callbackQuery) {
+    category = callbackQuery.data;
+
+    //remove event listener category callback
+    bot.removeListener("callback_query");
+
+    //scraping
+    initScraping(chatID);
+  });
+}
+
+function initScraping(chatID) {
+  let scraper = new Scraper(query, category, page_current);
+
+  scraper.FetchHtml().then(function (html) {
+    let last_page: number = Scraper.GetLastPage(html);
+    let links: string[] = scraper.ExtractLink(html);
+
+    //send links with timeout
+    SendDelayedMessages(links, chatID).then((res) => {
+      //creating paging
+      let pagine = {
+        reply_markup: JSON.stringify({
+          inline_keyboard: JSON.parse(
+            Scraper.CreateJsonPages(last_page, page_current)
+          ),
+        }),
+      };
+
+      if (page_current <= last_page) {
+        bot
+          .sendMessage(chatID, "Vuoi cambiare pagina?", pagine)
+          .then(function (res) {
+            bot.on(
+              "callback_query",
+              function onCallbackQueryPages(callbackQuery2) {
+                page_current = callbackQuery2.data;
+                //remove event listener category callback
+                bot.removeListener("callback_query");
+                initScraping(chatID);
+              }
+            );
+          });
+      }
+    });
+  });
+}
+
+/*  OOP version  */
+bot.onText(/\/cerca/, (msg, match) => {
+  bot.sendMessage(msg.chat.id, "Dimmi cosa cerchi").then(function () {
+    answerCallbacks[msg.chat.id] = function (answer) {
+      query = answer.text;
+
+      let options = {
+        reply_markup: JSON.stringify({
+          inline_keyboard: JSON.parse(Scraper.CreateJsonOption()),
+        }),
+      };
+
+      bot
+        .sendMessage(msg.chat.id, "Scegli una di queste categorie:", options)
+        .then(function (res) {
+          //funzione core
+          SetCategory(msg.chat.id);
+        });
+    };
+  });
+});
---
+++ src/scraper.ts
@@ -0,0 +1,145 @@
+import axios from "axios";
+const cheerio = require("cheerio");
+
+class Scraper {
+  //props
+  public query: string;
+  public category: string;
+  public page: number;
+  public url: string;
+  public options: object;
+  public links: string[];
+  public last_page: number;
+  public html: string;
+
+  //constructor
+  constructor(query: string, category?: string, page: number = 1) {
+    this.query = Scraper.CleanQuery(query);
+    this.category = category;
+    this.page = page;
+    this.url = `https://www.subito.it/annunci-italia/vendita/${this.category}/?q=${this.query}&o=${this.page}`;
+  }
+
+  public MakeUrl(): string {
+    return `https://www.subito.it/annunci-italia/vendita/${this.category}/?q=${this.query}`;
+  }
+  //clean input query
+  public static CleanQuery(query: string): string {
+    //trim starting/ending spaces
+    query = query.trim();
+
+    //replace empty spaces with + for query search
+    query = query.split(" ").join("+");
+
+    return query;
+  }
+
+  //create options inline json
+  public static CreateJsonOption(): string {
+    var arr_cat = [];
+
+    for (let item in ECategory) {
+      let cat_text = ECategory[item];
+
+      let cat_inline = [
+        {
+          text: cat_text,
+          callback_data: cat_text,
+          command: "categories",
+        },
+      ];
+
+      arr_cat.push(cat_inline);
+    }
+
+    return JSON.stringify(arr_cat);
+  }
+
+  //create user options
+  public CreateOptionsCategory(): void {
+    this.options = {
+      reply_markup: JSON.stringify({
+        inline_keyboard: JSON.parse(Scraper.CreateJsonOption()),
+      }),
+    };
+  }
+
+  public ExtractLink(html: string): string[] {
+    let links = [];
+    const $ = cheerio.load(html);
+    $(".items__item", html).each(function () {
+      let link = $(this).children("a").attr("href");
+      if (link) {
+        links.push(link);
+      }
+    });
+    return links;
+  }
+
+  public static GetLastPage(html: string): number {
+    const $ = cheerio.load(html);
+    let last_page = $(".unselected-page", html)
+      .last()
+      .children("a")
+      .attr("href");
+    last_page = last_page.split("&o=")[1];
+    return parseInt(last_page);
+  }
+
+  public SetLastPage(html: string): void {
+    const $ = cheerio.load(html);
+    let last_page = $(".unselected-page", html)
+      .last()
+      .children("a")
+      .attr("href");
+    last_page = last_page.split("&o=")[1];
+    this.last_page = parseInt(last_page);
+  }
+
+  public async FetchHtml(): Promise<string> {
+    let html = await axios.get(this.url);
+    return html.data;
+  }
+
+  public static CreatePagesArray(last_page: number): number[] {
+    let pages: number[] = [];
+    for (let index = last_page; index > 0; index--) {
+      pages.push(index);
+    }
+    pages.sort((a, b) => a - b);
+    return pages;
+  }
+
+  public static CreateJsonPages(
+    last_page: number,
+    page_current: number
+  ): string {
+    let pages: number[] = this.CreatePagesArray(last_page);
+    var arr_pages = [];
+
+    let pag_inline = [
+      {
+        text: page_current++ + 1,
+        prev_page: page_current - 1,
+        callback_data: page_current,
+        command: "pages",
+      },
+    ];
+
+    arr_pages.push(pag_inline);
+    return JSON.stringify(arr_pages);
+  }
+}
+
+enum ECategory {
+  Auto = "auto",
+  AccessoriAuto = "accessori-auto",
+  MotoScooter = "moto-e-scooter",
+  AccessoriMoto = "accessori-moto",
+
+  Elettronica = "elettronica",
+  CasaPersona = "casa-e-persona",
+  SportHobby = "sport-e-hobby",
+}
+
+export default Scraper;
